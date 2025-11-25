5641d00275334311ef872dd3389e996995a2b72d
Elias Gander <gander.elias@yahoo.de>
2025-11-26 00:35:00 +0100

    debug abdrehen

--- download-and-upload-trajectories/constants.py
+++ download-and-upload-trajectories/constants.py
@@ -4,4 +4,4 @@ MAPILLARY_PASSWORD = "cesjuD-6tyjmo-maqgif"
 EMAIL_ADDRESS = "tomturbo657@gmail.com"
 GMAIL_APP_PASSWORD = "kruf aahg aiuc iwtr"
 VOLUME_SIZE_IN_BYTES = 40 * 1024 * 1024 * 1024  # 40GB
-IS_DEBUG = True
+IS_DEBUG = False
